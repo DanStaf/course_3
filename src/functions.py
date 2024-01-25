52e9fbd9f1423dff36247465ce364fb4b2c4d4a0
DanStaf <danstaf@mail.ru>
2024-01-25 22:56:09 +0300

    files with blank functions created

---
+++ src/functions.py
@@ -0,0 +1,5 @@
+
+def get_5_operations():
+
+    pass
+
---
+++ tests/test_functions.py
@@ -0,0 +1,7 @@
+import pytest
+
+from functions import get_5_operations
+
+def test_get_5_operations():
+
+    assert get_5_operations() == True
