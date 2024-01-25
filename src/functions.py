710b9fa1eb5320d98465aebc2fb5745a3abacef7
DanStaf <danstaf@mail.ru>
2024-01-26 00:06:29 +0300

    read from file done

--- src/functions.py
+++ src/functions.py
@@ -1,5 +1,18 @@
+import os.path
+import json
 
 def get_5_operations():
 
-    pass
+    file_name = os.path.join('..', 'operations.json')
 
+    with open(file_name, 'r', encoding='utf-8') as f:
+        result = f.read()
+
+    data_json = json.loads(result)
+
+
+
+
+
+
+get_5_operations()
